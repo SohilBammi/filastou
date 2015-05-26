514fc4ad5c82f29d466921b4212b53920775a691
unknown <sohilbammi@berkeley.edu>
2015-05-26 13:27:10 -0700

    Android Complete w/ possible PHP errors

--- AndroidApp/app/src/main/java/com/filastou/filastou/app/AppConfig.java
+++ AndroidApp/app/src/main/java/com/filastou/filastou/app/AppConfig.java
@@ -4,4 +4,9 @@ package com.filastou.filastou.app;
  * Created by Sohil on 5/25/2015.
  */
 public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://107.184.143.193/android_login_api/";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://107.184.143.193/android_login_api/";
 }
